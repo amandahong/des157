68f95b5a07b1d8e41c89bbd004b1670684f98dd7
Amanda Hong <ahong425@scc2102-14.ucdavis.edu>
2017-01-17 14:31:57 -0800

    in class studio (r1)

---
+++ script.js
@@ -0,0 +1,59 @@
+// p5 file
+
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log("hi");
+
+//two pupils
+var rEyeX = 220;
+var rEyeY = 200;
+var rEyeXCur = rEyeX;
+var rEyeYCur = rEyeY;
+
+var lEyeX = 380;
+var lEyeY = 200;
+var lEyeXCur = lEyeX;
+var lEyeYCur = lEyeY;
+
+var shift = 10;
+var baseColor = 252;
+
+
+function setup() {
+  //putting this canvas in a variable
+  var myCanvas=createCanvas(800, 250);
+  myCanvas.parent('mySketch')
+  background(252);
+}
+
+
+function draw()
+{
+//adds location interaction for pupils with mouse location
+  var ptrDist = dist(rEyeX,rEyeY, mouseX, mouseY);
+  var ratio = shift / ptrDist;
+  rEyeXCur = ratio * (mouseX - rEyeX) + rEyeX;
+  rEyeYCur = ratio * (mouseY - rEyeY) + rEyeY;
+  var ptrDist = dist(lEyeX,lEyeY, mouseX, mouseY);
+  var ratio = shift / ptrDist;
+  lEyeXCur = ratio* (mouseX - lEyeX) + lEyeX;
+  lEyeYCur = ratio* (mouseY - lEyeY) + lEyeY;
+//draws out the face with eyes
+  ellipse(300,250, 400, 400);
+  fill(255);
+  ellipse(220,200, 80, 50);
+  ellipse(380,200, 80, 50);
+  fill(0);
+//draws out pupils
+  ellipse(rEyeXCur,rEyeYCur, 30, 30);
+  ellipse(lEyeXCur,lEyeYCur, 30, 30);
+//draws a line
+  line(pmouseX, pmouseY, mouseX, mouseY);
+}
+
+/*
+function mouseIsPressed()
+{
+
+}
+*/

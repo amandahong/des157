2f0b997bc0d463e66fd84add9b2a7634def55140
Amanda Hong <amandahong@gmail.com>
2017-01-24 15:07:35 -0800

    in class 1/24
    add repository and studio 2

---
+++ studio2/script.js
@@ -0,0 +1,13 @@
+// p5 file
+// studio 2 JS
+// open the console window of any browser to see console.log() messages,
+// like print() messages in Processing
+console.log("hi");
+
+
+/*
+function mouseIsPressed()
+{
+
+}
+*/

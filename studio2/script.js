1bddf85d7fb85e21fd65da51ad995ebfc196733d
Amanda Hong <ahong425@scc2102-20.ucdavis.edu>
2017-01-24 16:27:50 -0800

    in class studio 2 work
    started studio 2 with code pen

--- studio2/script.js
+++ studio2/script.js
@@ -4,7 +4,31 @@
 // like print() messages in Processing
 console.log("hi");
 
+//capture the submit event
+  document.f.onsubmit = processForm;
 
+  //define process function
+  function processForm() {
+
+    //store user name in a variable
+    var userName = document.f.userName.value;
+   // alert('Hi ' + userName);
+
+    //store userColor in a variable called userColor
+
+    var userColor = document.f.userColor.value;
+
+    var myMsg = document.getElementById('myMsg');
+
+    myMsg.innerHTML= ('Hi ' + userName + ' I also like ' + userColor);
+    //comment out the alert message above
+
+    //call a new alert() to concatenate a message with userName and userColor
+
+     //prevent page from reloading
+    return false;
+
+  }
 /*
 function mouseIsPressed()
 {

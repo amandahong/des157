3c79061618e62219afef1035ee9d9ad97985b72f
Amanda Hong <amandahong@gmail.com>
2017-01-26 15:09:29 -0800

    studio 2 work (r1)

--- studio2/script.js
+++ studio2/script.js
@@ -13,20 +13,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
     function processForm() {
 
         //store names in variables
-        var userName = document.f.userName.value;
+        var adj1 = document.f.adj1.value;
 
-        var userColor = document.f.userColor.value;
+        var famous = document.f.famous.value;
+
+        var verb = document.f.verb.value;
+
+        var song = document.f.song.value;
+
+        var adj2 = document.f.adj2.value;
+
+        var adj3 = document.f.adj3.value;
+
+        var noun = document.f.noun.value;
 
         var myMsg = document.getElementById('myMsg');
 
 
-        /*
+
             //print out into message
-            myMsg.innerHTML = 'Hi ' + userName + ' I also like ' + userColor;
+            myMsg.innerHTML = 'Brian Imanuel, also known as Rich Chigga is a ' + adj1 + ' up and coming hip hop artist, hailing from Jakarta, Indonesia. He gets most of his inspiration from ' + famous + ' and is known to ' + verb + ' to ' + song + ' as an artistic pastime. Rich Chigga is also a/an ' + adj2 + ' comedian, often posting humorous bits to Twitter, such as, “My moves are ' + adj3 + ' dont even try” and “did U just disrespect my ' + noun + '?".' + ' <br><br>  Here is a final quote from Rich Chigga: “Dont comment "i love you" on my instagram. u dont even know what love is. u dont know what it feels like.”'
+
+            ;
             myMsg.className = "show";
-        */
 
 
+    /*
         if (userName = "" || userColor = "") {
             alert('please fill out!');
         } else {
@@ -34,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             myMsg.innerHTML = 'Hi ' + userName + ' I also like ' + userColor;
 
         }
-
+  */
         //prevent page from reloading
         return false;
 

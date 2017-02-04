c4c3529a59f32adf6e05e58998c3f8e88d7cb432
Amanda Hong <amandahong@gmail.com>
2017-02-03 18:16:34 -0800

    studio 3 work

--- studio3/script.js
+++ studio3/script.js
@@ -3,11 +3,18 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
+    var bag = document.getElementById('bag');
     var pen = document.getElementById('pen');
     var book = document.getElementById('book');
-    var close = document.getElementsByClassName('close');
-    var items = [pen, book];
+    var charger = document.getElementById('charger');
+    var tea = document.getElementById('tea');
+    var earphone = document.getElementById('earphone');
 
+    var close = document.getElementsByClassName('close');
+
+    var items = [bag, pen, book, charger, tea, earphone];
+
+//loop to assign info boxes to pictures and show on click
     for (a = 0; a < items.length; a++) {
         items[a].addEventListener('click', function() {
             var id = this.getAttribute('id') + 'Info';
@@ -18,18 +25,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
         })
     }
 
-
+//loop to assign the close function to the X
     for (i = 0; i < close.length; i++) {
         close[i].addEventListener('click', function() {
             this.parentElement.style.display = "none";
             //parent element is the aside box because close is nested within the aside
         })
     }
-
-
-    //book
-
-
-
 
 });
